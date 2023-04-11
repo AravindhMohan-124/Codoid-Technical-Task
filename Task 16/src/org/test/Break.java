9fb36d9c0f18c02278192b67d06d7a5387b68a9d
Aravindh <aravindhmohan@gmail.com>
2023-04-11 20:08:10 +0530

    First Commit

---
+++ Task 16/src/org/test/Break.java
@@ -0,0 +1,33 @@
+package org.test;
+
+public class Break {
+public static void main(String[] args) {
+	
+	int i=0;
+	
+	while (i<10) {
+	
+	System.out.println(i);	
+	i++;	
+		
+	continue;
+	
+	
+	}
+	
+	
+	System.out.println("END");
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+}
---
+++ Task 16/src/org/test/Continue.java
@@ -0,0 +1,38 @@
+package org.test;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		
+	int i=0;
+    while (i<10) {
+	System.out.println(i);	
+    	
+    i++;	
+    
+    break;
+	}
+		
+		
+	System.out.println("END");	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
